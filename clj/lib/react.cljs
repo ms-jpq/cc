f1df97edb1bfa677c51f2a2fdc39b7673353a60c
root <hao@graveflex.com>
2023-05-29 16:38:23 -0700

    recon

--- clj/lib/react.cljs
+++ clj/lib/react.cljs
@@ -18,6 +18,11 @@
       (name)
       (s/replace re-case #(let [[_ m] %] (-> m str s/capitalize)))))
 
+(defn- long-zip [bottom & seqs]
+  (let [len (apply max (map count seqs))
+        longer (map #(take len (concat % (repeat bottom))) seqs)]
+    (apply map vector (range) longer)))
+
 (defn- parse-kw [kw]
   {:pre [(keyword? kw)]}
   (let [key (name kw)
@@ -32,7 +37,7 @@
 
 (defmulti parse discrim)
 (defmethod parse :nil [_] nil)
-(defmethod parse :str [s] s)
+(defmethod parse :str [s] {:txt s})
 (defmethod parse :seq [[x & xs :as xss]]
   (if (not (keyword? x))
     (->> xss (map parse) (remove empty?))
@@ -41,7 +46,7 @@
                                         #(if (map? %) :r-props :r-children)
                                         xs)
           {:keys [tag key style] :as props} (->> r-props (apply merge-with into) (merge kw-props))
-          children (->> r-children (map parse) (remove empty?))]
+          children (->> r-children (map parse) (remove empty?) (into []))]
       {:tag tag
        :key key
        :props (dissoc props :tag :key :style)
@@ -49,52 +54,74 @@
        :children children})))
 
 (defn- do-draw [target props]
-  (doseq [[key val] props]
-    (let [js-key (js-case key)]
-      (if (nil? val)
-        (js-delete target js-key)
-        (aset target js-key val)))))
+  (doseq [[key val] props
+          :let [js-key (js-case key)]]
+    (if (nil? val)
+      (js-delete target js-key)
+      (aset target js-key val))))
 
 (defmulti draw discrim)
 (defmethod draw :seq [s] (map draw s))
-(defmethod draw :str [s] (. js/document createTextNode s))
-(defmethod draw :map [{:keys [tag props style children]}]
-  (let [el (. js/document createElement tag)
-        el-style (.-style el)]
-    (do-draw el props)
-    (do-draw el-style style)
-    (doseq [child children]
-      (let [c (draw child)]
-        (. el appendChild c)))
-    el))
+(defmethod draw :map [{:keys [txt tag props style children]}]
+  (if txt
+    (. js/document createTextNode txt)
+    (let [el (. js/document createElement tag)
+          el-style (.-style el)]
+      (do-draw el props)
+      (do-draw el-style style)
+      (doseq [child children
+              :let [c (draw child)]]
+        (. el appendChild c))
+      el)))
 
 (defn- do-recon [target old-props new-props]
-  (doseq [[new-key new-val] new-props]
-    (let [old-val (new-key old-props)]
-      (when (not (= old-val new-val))
-        (let [js-key (js-case new-key)]
-          (if (nil? new-val)
-            (js-delete target js-key)
-            (aset target js-key new-val))))))
-  (doseq [[old-key] old-props]
-    (when (not (contains? old-key new-props))
-      (let [js-key (js-case old-key)]
-        (js-delete target js-key)))))
+  (doseq [[new-key new-val] new-props
+          :let [old-val (new-key old-props)]
+          :when (not (= old-val new-val))
+          :let [js-key (js-case new-key)]]
+    (if (nil? new-val)
+      (js-delete target js-key)
+      (aset target js-key new-val)))
+  (doseq [[old-key] old-props
+          :when (not (contains? old-key new-props))
+          :let [js-key (js-case old-key)]]
+    (js-delete target js-key)))
 
-(defn- do-rec [old-children new-children]
+(defn- recon [{old-txt :txt old-tag :tag :as old}
+              {new-txt :txt new-tag :tag :as new}]
+  {:pre [(not (nil? old)) (not (nil? new))]}
+  (cond
+    (or old-txt new-txt) (if (= old-txt new-txt) old (assoc new :el (draw new)))
 
-  new-children)
+    (not (= old-tag new-tag)) (assoc new :el (draw new))
 
-(defn- recon [{old-tag :tag :as old} {new-tag :tag :as new}]
-  (if (not (= old-tag new-tag))
-    (let [el (draw new)]
-      (assoc new :el el))
-    (let [{:keys [el] old-props :props old-style :style old-children :children} old
-          {new-props :props new-style :style new-children :children} new
-          el-style (.-style el)]
-      (do-recon el old-props new-props)
-      (do-recon el-style old-style new-style)
-      (assoc new :el el))))
+    :else (let [{:keys [el] old-props :props old-style :style old-children :children} old
+                {new-props :props new-style :style new-children :children} new
+                el-style (.-style el)]
+            (do-recon el old-props new-props)
+            (do-recon el-style old-style new-style)
+            (let [zipped (long-zip nil old-children new-children)
+                  keyed (if (contains? (first new-children) :key)
+                          (->> old-children
+                               (map-indexed #(vector (:key %2) [%1 %2]))
+                               (remove (comp nil? first))
+                               (into {}))
+                          {})
+                  c-iter (for [[idx old-child new-child] zipped
+                               :let [{old-key :key} old-child
+                                     {new-key :key} new-child]]
+                           (cond
+                             (= old-key new-key) (cond
+                                                   (nil? old-child) (let [new-el (draw new-child)]
+                                                                      (assoc new-child :el new-el)
+                                                                      (.appendChild el new-el))
+                                                   (nil? new-child) (do
+                                                                      (.remove (:el old-child))
+                                                                      nil)
+                                                   :else (recon old-child new-child))
+                             :else (assert false)))
+                  children (doall (remove nil? c-iter))]
+              (assoc new :el el :children children)))))
 
 (defn rend [v-init]
   {:pre [(map? v-init)]}
@@ -114,8 +141,8 @@
            "wwwwwwwwww"
            {:style {:background-color "green"}} {:style {:color "pink"}}
            {:style {:color nil}}
-           [:h1 "h1"]
-           [:h3 "H3"]
+           [:h1 "big-txt" {:key 9}]
+           [:h3 "somt-txt"]
            {:data-stdin "somthing funny"}])
 
 (. js/console clear)
@@ -123,7 +150,10 @@
 (def r (rend p))
 (def it (r p))
 
+(. js/console log "---------------------")
 (p/pprint p)
 (. js/console log it)
+
+(p/pprint (if "" 1 2))
 
 ; (-> js/document (.  -body) (. appendChild it))
