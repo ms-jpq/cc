7a5cba78d206a659d5a142f2657f02a1f69fb57d
dogisgreat <github@bigly.dog>
2024-11-03 14:03:42 -0500

    clj

--- clj/srv/main.clj
+++ clj/srv/main.clj
@@ -2,48 +2,13 @@
   (:require
    [clojure.data.json :refer [read-str]]
    [clojure.tools.cli :refer [parse-opts]]
-   [clojure.tools.logging :as log])
-  (:import
-   [com.sun.net.httpserver HttpExchange HttpHandler HttpServer]
-   [java.net InetSocketAddress]
-   [java.nio.file Files]))
+   [clojure.tools.logging :as log]
+   [srv.server :refer [run]]))
 
-(defn make-handler [handle-f]
-  {:pre [(fn? handle-f)]}
-  (reify HttpHandler
-    (handle [_ exchange]
-      {:pre [(instance? HttpExchange exchange)]}
-      (try
-        (let [response (handle-f exchange)
-              response-bytes (.getBytes response)
-              response-length (alength response-bytes)]
-          (.sendResponseHeaders exchange 200 response-length)
-          (with-open [os (.getResponseBody exchange)]
-            (.write os response-bytes)
-            (.flush os)))
-        (catch Exception e
-          (println "Error handling request:" (.getMessage e)))
-        (finally
-          (.close exchange))))))
-
-(defn router [exchange]
-  {:pre [(instance? HttpExchange exchange)]}
-  (let [uri (str (.getRequestURI exchange))
-        method (.. exchange getRequestMethod toUpperCase)]
-    (case [method uri]
-      ["GET" "/"] "Welcome to the homepage!"
-      ["GET" "/about"] "This is the about page"
-      ["POST" "/api"] "API endpoint"
-      "404 Not Found")))
-
-(defn run-server [port]
-  (let [addr (InetSocketAddress. port)
-        server (HttpServer/create addr 0)]
-    (-> server
-        (.createContext "/" (make-handler router))
-        (.setExecutor nil)
-        (.start))
-    server))
+(defn- handler [request]
+  {:pre [(map? request)]}
+  (println request)
+  {:status 200})
 
 (defn -main [& args]
   {:pre [(seq? args)]}
@@ -51,7 +16,7 @@
               ["-r" "--root ROOT"
                :default (System/getProperty "user.dir")]
               ["-p" "--port PORT"
-               :default 80
+               :default 8080
                :parse-fn #(Integer/parseInt %)
                :validate [#(< 0 % 0x10000)]]]
         {{:keys [help port root]} :options
@@ -60,5 +25,4 @@
     (cond help (println summary)
           errors ((doseq [e errors] (log/error e))
                   (System/exit 2))
-          :else (println {:port port
-                          :root root}))))
+          :else (run port handler))))
--- clj/srv/server.clj
+++ clj/srv/server.clj
@@ -1,1 +1,75 @@
-(ns srv.server)
+(ns srv.server
+  (:require [clojure.string :as str])
+  (:import
+   [com.sun.net.httpserver HttpExchange HttpHandler HttpServer]
+   [java.net InetSocketAddress URLDecoder]
+   [java.nio.charset StandardCharsets]
+   [java.util.concurrent Executors]
+   ))
+
+(def ^:private utf-8 StandardCharsets/UTF_8)
+
+(defn- uri-decode [txt]
+  {:pre [(string? txt)]}
+  (try
+    (URLDecoder/decode txt utf-8)
+    (catch Exception _ txt)))
+
+(defn- parse-query [query-string]
+  {:pre [(string? query-string)]}
+  (->> (str/split query-string #"&")
+       (remove str/blank?)
+       (map #(let [[k v] (map uri-decode (str/split % #"=" 2))]
+               [[(keyword k) v]]))
+       (into {})))
+
+(defn- parse-exchange [exchange]
+  {:pre [(instance? HttpExchange exchange)]}
+  (let [local-addr (.getLocalAddress exchange)
+        remote-addr (.getRemoteAddress exchange)
+        http-version (.getProtocol exchange)
+        method (.. exchange getRequestMethod toUpperCase)
+        uri (.getRequestURI exchange)
+        query (-> uri .getQuery parse-query)
+        headers (-> exchange .getRequestHeaders
+                    (into {}))
+        body (.getRequestBody exchange)]
+    {:local-addr local-addr
+     :remote-addr remote-addr
+     :http-version http-version
+     :method method
+     :path (.getPath uri)
+     :query query
+     :headers headers
+     :body body}))
+
+(defn- make-handler [process]
+  {:pre [(fn? process)]}
+  (reify HttpHandler
+    (handle [_ exchange]
+      {:pre [(instance? HttpExchange exchange)]}
+      (try
+        (let [request (parse-exchange exchange)
+              response (process request)
+              {:keys [status headers body]} response]
+          (doto exchange
+            (.getResponseHeaders (doseq [[k v] headers] (.add k v)))
+            (.sendResponseHeaders (or status 200) 0)
+            (.. getResponseBody (write body))))
+        (catch Exception e
+          (let [msg (-> e .getMessage .getBytes)]
+            (doto exchange
+              (.sendResponseHeaders 500 0)
+              (.. getResponseBody (write msg)))))
+        (finally
+          (.close exchange))))))
+
+(defn run [port process]
+  {:pre [(int? port) (fn? process)]}
+  (let [addr (InetSocketAddress. port)
+        server (HttpServer/create addr 0)]
+    (doto server
+        (.createContext "/" (make-handler process))
+        (.setExecutor (Executors/newCachedThreadPool))
+        (.start))
+    server))
