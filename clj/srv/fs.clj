8b5a5b93d9765f35b51c7cdd55b71483a226ed7c
dogisgreat <github@bigly.dog>
2024-11-10 20:06:37 -0500

    add max depth

--- clj/srv/fs.clj
+++ clj/srv/fs.clj
@@ -3,7 +3,7 @@
    [lib.interop :as ip]
    [lib.prelude :as lib])
   (:import
-   [java.nio.file FileSystems Files LinkOption Path]
+   [java.nio.file Files LinkOption Path NoSuchFileException]
    [java.util.stream Stream]))
 
 (def path? (partial instance? Path))
@@ -16,18 +16,20 @@
   {:pre [(path? path)]}
   (.toRealPath path (into-array LinkOption [])))
 
-(defn- attributes [path]
+(defn stat [path]
   {:pre [(path? path)]
-   :post [(map? %)]}
-  (as-> path $
-    (Files/readAttributes $ "posix:*" (into-array LinkOption [LinkOption/NOFOLLOW_LINKS]))
-    (map (fn [[k v]] [(-> k lib/clj-case keyword) v]) $)
-    (into {} $)))
+   :post [((some-fn map? nil?) %)]}
+  (try
+    (as-> path $
+      (Files/readAttributes $ "posix:*" (into-array LinkOption [LinkOption/NOFOLLOW_LINKS]))
+      (map (fn [[k v]] [(-> k lib/clj-case keyword) v]) $)
+      (into {} $))
+    (catch NoSuchFileException _ nil)))
 
 (defn- parse [path]
   {:pre [(path? path)]
    :post [(map? %)]}
-  (let [{:keys [is-symbolic-link is-directory size last-modified-time creation-time]} (attributes path)]
+  (let [{:keys [is-symbolic-link is-directory size last-modified-time creation-time]} (stat path)]
     {:path path
      :link (when is-symbolic-link
              (canonicalize path))
@@ -36,41 +38,40 @@
      :m-time last-modified-time
      :c-time creation-time}))
 
-(defn- stream-dir [dir]
-  {:pre [(path? dir)]}
-  (let [que (atom [])
-        st (-> dir
-               Files/list
-               (.filter (ip/->pred #(Files/isReadable %)))
-               (.map (ip/->fn #(let [{:keys [dir?]
-                                      :as parsed} (parse %)]
-                                 (when dir?
-                                   (swap! que conj %))
-                                 parsed)))
-               Stream/of
-               (.flatMap (ip/->fn identity)))
-        gen (Stream/generate
-             (ip/->supp #(let [path (peek @que)]
-                           (when-not (nil? path)
-                             (swap! que pop))
-                           path)))
-        st2 (.. gen
-                (takeWhile (ip/->pred (complement nil?)))
-                (flatMap (ip/->fn stream-dir)))]
-    (Stream/concat st st2)))
+(defn- stream-dir [max-depth depth dir]
+  {:pre [(int? max-depth) (int? depth) (path? dir)]}
+  (if (>= depth max-depth)
+    (Stream/empty)
+    (let [que (atom [])
+          st (-> dir
+                 Files/list
+                 (.filter (ip/->pred #(Files/isReadable %)))
+                 (.map (ip/->fn #(let [{:keys [dir?]
+                                        :as parsed} (parse %)]
+                                   (when dir?
+                                     (swap! que conj %))
+                                   parsed)))
+                 Stream/of
+                 (.flatMap (ip/->fn identity)))
+          gen (Stream/generate
+               (ip/->supp #(let [path (peek @que)]
+                             (when-not (nil? path)
+                               (swap! que pop))
+                             path)))
+          st2 (.. gen
+                  (takeWhile (ip/->pred (complement nil?)))
+                  (flatMap (ip/->fn (partial stream-dir max-depth (inc depth)))))]
+      (Stream/concat st st2))))
 
-(defn walk [root dir]
-  {:pre [(path? root) (path? dir)]}
-  (let [real-root (canonicalize root)]
-    (-> dir
-        canonicalize
-        stream-dir
-        (.filter (ip/->pred (comp (some-fn nil? #(.startsWith % real-root)) :link))))))
+(defn walk [max-depth root dir]
+  {:pre [(int? max-depth) (path? root) (path? dir)]}
+  (let [real-root (canonicalize root)
+        pred (ip/->pred (comp (some-fn nil? #(.startsWith % real-root)) :link))]
+    (.filter (stream-dir max-depth 0 (canonicalize dir)) pred)))
 
 (defn glob [root dir pattern]
   {:pre [(string? pattern)]}
   (let [fs (.getFileSystem root)
-        matcher (->> pattern (str "glob:" dir (.getSeparator fs)) (.getPathMatcher fs))]
-    (-> dir
-        (walk root)
-        (.filter (ip/->pred (comp #(.matches matcher %) :path))))))
+        matcher (->> pattern (str "glob:" dir (.getSeparator fs)) (.getPathMatcher fs))
+        pred (ip/->pred (comp #(.matches matcher %) :path))]
+    (.filter (walk Long/MAX_VALUE root dir) pred)))
--- clj/srv/index.clj
+++ clj/srv/index.clj
@@ -8,21 +8,44 @@
 
 (def path-glob (str (UUID/randomUUID)))
 
+(defn- rel-path [root {:keys [dir? path]}]
+  {:pre [(fs/path? root) (boolean? dir?) (fs/path? path)]}
+  (let [rel (-> root (.relativize path) str)]
+    (if dir? (str rel "/") rel)))
+
 (defn handler-glob [root data-dir {:keys [path query]}]
   {:pre [(fs/path? root) (fs/path? data-dir)]}
-  {:body ""})
-
-(defn handler-static [root data-dir {:keys [path]}]
-  {:pre [(fs/path? root) (fs/path? data-dir)]}
-  (let [dir (.resolve root path)
-        st (fs/walk root dir)]
+  (let [pattern (-> query :q first)
+        dir (.resolve root path)
+        st (fs/glob root dir pattern)]
     {:close st
      :body (h/html
             [:main [:ul
-                    (for [{:keys [path dir? size m-time c-time]} (ip/st->seq st)
-                          :let [rel (-> root (.relativize path) str)]]
+                    (for [{:keys [size m-time c-time]
+                           :as row} (ip/st->seq st)
+                          :let [rel (rel-path root row)]]
                       [:li
                        [:a {:href rel} rel]
                        [:span size]
                        [:span (str c-time)]
                        [:span (str m-time)]])]])}))
+
+(defn handler-static [root data-dir {:keys [path query]}]
+  {:pre [(fs/path? root) (fs/path? data-dir)]}
+  (let [current (.resolve root path)
+        {:as attrs} (fs/stat current)]
+    (if (nil? attrs)
+      {:status 404
+       :body "404"}
+      (let [st (fs/walk 1 root current)]
+        {:close st
+         :body (h/html
+                [:main [:ul
+                        (for [{:keys [size m-time c-time]
+                               :as row} (ip/st->seq st)
+                              :let [rel (rel-path root row)]]
+                          [:li
+                           [:a {:href rel} rel]
+                           [:span size]
+                           [:span (str c-time)]
+                           [:span (str m-time)]])]])}))))
