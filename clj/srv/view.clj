f98967ba2b775560c8e280fb1936015e27e34e89
dogisgreat <github@bigly.dog>
2024-11-16 16:38:33 -0500

    clj

--- clj/srv/view.clj
+++ clj/srv/view.clj
@@ -1,35 +1,41 @@
 (ns srv.view
   (:require
+   [clojure.java.io :as io]
    [clojure.string :as str]
    [lib.interop :as ip]
+   [lib.prelude :as lib]
    [srv.fs :as fs])
   (:import [java.io FileInputStream]
            [java.net URLConnection]
            [java.nio ByteBuffer]
            [java.security MessageDigest]
-           [java.time Instant]))
+           [java.time Instant]
+           [java.util UUID]))
+
+(def ^:private resource-path (str (UUID/randomUUID)))
 
 (defn- l->b [long]
   {:pre [(int? long)]}
   (-> (ByteBuffer/allocate 8) (.putLong long) .array))
 
+(defn- md5 [& bs]
+  (let [hasher (MessageDigest/getInstance "MD5")]
+    (doseq [b bs]
+      (.update hasher b))
+    (->> hasher
+         .digest
+         (map (partial format "%02x"))
+         (str/join ""))))
+
 (defn- file-headers [{:keys [path size m-time]}]
   {:pre [(ip/path? path) (int? size) (instance? Instant m-time)]}
-  (let [md (MessageDigest/getInstance "MD5")]
-    (doto md
-      (.update (-> path str .getBytes))
-      (.update (l->b size))
-      (.update (l->b (.toEpochMilli m-time))))
-    {:content-type (-> path
-                       .toFile
-                       .getName
-                       URLConnection/guessContentTypeFromName
-                       (or "application/octet-stream"))
-     :etag (->> md
-                .digest
-                (map (partial format "%02x"))
-                (str/join ""))
-     :accept-ranges "bytes"}))
+  {:content-type (-> path
+                     .toFile
+                     .getName
+                     URLConnection/guessContentTypeFromName
+                     (or "application/octet-stream"))
+   :etag (md5 (-> path str .getBytes) (l->b size) (l->b (.toEpochMilli m-time)))
+   :accept-ranges "bytes"})
 
 (defn- stream-file [path pos]
   {:pre [(ip/path? path) (string? pos)]}
@@ -40,7 +46,9 @@
                 :as _}]
   {:pre [(ip/path? root) (ip/path? data-dir)]}
   (let [{:keys [if-none-match range]} headers
-        current (.resolve root path)
+        current (if (str/starts-with? path resource-path)
+                  (io/resource (lib/remove-prefix path resource-path))
+                  (.resolve root path))
         {:keys [link dir?]
          :or {link (ip/path "/")}
          :as attrs} (fs/stat current)
